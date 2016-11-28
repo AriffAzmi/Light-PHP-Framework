2164a1885832a8ad00915446081cf2e6bc665c89
AriffAzmi <ariff.azmi16@gmail.com>
2016-11-28 20:17:26 +0800

    Update Model.php

--- Pfw/App/Pfw/Model.php
+++ Pfw/App/Pfw/Model.php
@@ -311,6 +311,249 @@
 		{
 			return DB::db_connect();
 		}
+		
+		public function find()
+		{
+			$data = get_object_vars($this);
+
+			/**
+			*
+			* @return affected column
+			* @var string/integer
+			*/
+			$column = array_keys(array_slice($data,1));
+			$column = $column[0];
+
+			/**
+			*
+			* @return bind column name
+			*/
+			$columnBind = ":$column";
+
+			/**
+			*
+			* @return data belongs to the column
+			*/
+			$columnData = array_values(array_slice($data,1));
+			$columnData = $columnData[0];
+
+			/**
+			*
+			* Statement to delete
+			* @var string
+			*/
+			$statement = "SELECT COUNT(*) AS TOTAL FROM $this->table WHERE ".$column."=".$columnBind."";
+
+			/**
+			*
+			* @return array bind parameter
+			*/
+			$arrayBind = [
+				$columnBind => $columnData
+			];
+
+			$prepare = DB::db_connect()->prepare($statement);
+			$prepare->execute($arrayBind);
+
+			$prepareData = $prepare->fetchAll(PDO::FETCH_OBJ);
+
+			return $prepareData[0];
+		}
+
+		public function all()
+		{
+			
+			$statement = "SELECT * FROM $this->table";
+
+			$prepare = DB::db_connect()->prepare($statement);
+			$prepare->execute();
+
+			$prepareData = $prepare->fetchAll(PDO::FETCH_OBJ);
+
+			return $prepareData;
+		}
+
+		public function query($statement=null,$arrayBind=null,$method=null)
+		{
+			$prepare = DB::db_connect()->prepare($statement);
+			$prepare->execute($arrayBind);
+
+			if ($prepare->execute($arrayBind)) {
+				
+				if ($method=='select') {
+					
+					$prepareData = $prepare->fetchAll(PDO::FETCH_OBJ);
+
+					return $prepareData;
+				}
+				else{
+
+					return true;
+				}
+			}
+		}
+
+		public function allWithClause()
+		{
+			$data = get_object_vars($this);
+
+			$value1 = implode(",", array_values(array_slice($data,1)));
+			
+			/**
+			*
+			* @return return affected column
+			* @var string/integer
+			*/
+			$keyTable = implode(",", array_keys(array_slice($data,1)));
+			$keyBind = implode(",:", array_keys(array_slice($data,1)));
+			$keyBind = ":".$keyBind;
+
+
+			/**
+			*
+			* @return array for bind paramater
+			*/
+			$groups_array = array_map('trim',explode(',', $keyBind));
+			$functions_array = array_map('trim',explode(',', $value1));
+
+			$final = array();
+			for ($i = 0; $i <= count($groups_array); $i++) {
+			    $final[$groups_array[$i]] = $functions_array[$i];
+			}
+
+			$bindParam = array_combine($groups_array, $functions_array);
+
+
+			$whereClause="";
+			foreach ($data as $key => $value) {
+				
+				if (count(array_keys($data)) <= 2) {
+
+					if ($key!='table') {
+						
+						$whereClause .= "$key=:$key";
+					}
+				}
+				else{
+
+					if($key!='table'){
+
+						$whereClause .= "$key=:$key,";
+					}
+				}
+			}
+
+			$whereClause = rtrim($whereClause,",");
+
+			$statement = "SELECT * FROM $this->table WHERE $whereClause";
+
+			$prepare = DB::db_connect()->prepare($statement);
+			$prepare->execute($bindParam);
+
+			$prepareData = $prepare->fetchAll(PDO::FETCH_OBJ);
+
+			return $prepareData;
+		}
+
+		public function totalCount()
+		{
+			$data = get_object_vars($this);
+
+			$value = implode(",", array_values(array_slice($data,1)));
+			$key = implode(",", array_keys(array_slice($data,1)));
+
+			// return $value;
+
+			$statement = "SELECT COUNT(*) AS TOTAL FROM $this->table WHERE $key =:$key LIMIT 1";
+
+			$prepare = DB::db_connect()->prepare($statement);
+			$prepare->execute([
+				":$key" => $value
+			]);
+
+			$prepareData = $prepare->fetchAll(PDO::FETCH_OBJ);
+
+			return $prepareData;
+		}
+
+		/**
+		*
+		* @return function to save data
+		*/
+		public function update()
+		{
+			$data = get_object_vars($this);
+
+			$getUniqueKeys = self::connection()->prepare(
+				"SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS TC WHERE TC.TABLE_NAME = '$this->table' AND TC.CONSTRAINT_TYPE = 'UNIQUE'"
+			);
+
+			$getUniqueKeys->execute();
+			$getUniqueKeysData = $getUniqueKeys->fetchAll(PDO::FETCH_OBJ);
+
+			if (array_key_exists($getUniqueKeysData[0]->CONSTRAINT_NAME, $data)) {
+				
+				$value1 = implode(",", array_values(array_slice($data,1)));
+
+				/**
+				*
+				* @return return affected column
+				* @var string/integer
+				*/
+				$keyTable = implode(",", array_keys(array_slice($data,1)));
+				$keyBind = implode(",:", array_keys(array_slice($data,1)));
+				$keyBind = ":".$keyBind;
+
+
+				$bindField = "VALUES (".$keyBind.")";
+				$tableField = "(".$keyTable.")";
+
+				/**
+				*
+				* @return array for bind paramater
+				*/
+				$groups_array = array_map('trim',explode(',', $keyBind));
+				$functions_array = array_map('trim',explode(',', $value1));
+
+				$final = array();
+				for ($i = 0; $i <= count($groups_array); $i++) {
+				    $final[$groups_array[$i]] = $functions_array[$i];
+				}
+
+				$bindParam = array_combine($groups_array, $functions_array);
+				
+				/**
+				*
+				* Statement to insert or update
+				*/
+				$statementInsertOrUpdate="";
+				$getExistingRecordStatement="UPDATE $this->table SET ";
+				foreach ($data as $key => $value) {
+					
+					if ($key!='table' && $key!=$getUniqueKeysData[0]->CONSTRAINT_NAME) {
+						$statementInsertOrUpdate .= "$key=:$key,";
+						$getExistingRecordStatement .= "$key=:$key,";
+					}
+				}
+
+				$statementInsertOrUpdate = rtrim($statementInsertOrUpdate, ",");
+				$getExistingRecordStatement = rtrim($getExistingRecordStatement, ",");
+				$getExistingRecordStatement .= " WHERE ".$getUniqueKeysData[0]->CONSTRAINT_NAME."=:".$getUniqueKeysData[0]->CONSTRAINT_NAME;
+
+				// return [$getExistingRecordStatement,$bindParam];
+	  			/**
+				*
+				* @return true/false
+				*/
+				return Model::execute($getExistingRecordStatement,$bindParam);
+			}
+			else{
+
+				// echo "Not found";
+			}
+
+
+		}
 
 	}
 ?>
